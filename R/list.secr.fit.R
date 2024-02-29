5d7e8cc93ab5feab0128838297f2eae5d2e2b285
Murray Efford <murray.efford@otago.ac.nz>
2024-02-29 22:30:02 +0000

    version 4.6.6

---
+++ R/list.secr.fit.R
@@ -0,0 +1,18 @@
+################################################################################
+## package 'secr'
+## list.secr.fit.R
+## 2024-02-19 supercedes par.secr.fit
+################################################################################
+
+## keeping it simple
+list.secr.fit <- function (..., constant = list(), prefix = "fit", names = NULL) {
+    fits <- mapply(secr.fit, ..., MoreArgs = constant, SIMPLIFY = FALSE)
+    nfits <- length(fits)
+    defaultnames <- paste0(prefix, 1:nfits)
+    if (is.null(names)) names <- defaultnames
+    else if (length(names) != nfits) {
+        warning ("number of names does not equal number of fits")
+        names <- defaultnames
+    }
+    secrlist(fits, names = names)
+}
